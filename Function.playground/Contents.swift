9de7c0bafc910125a02897863475ef420edc704a
Bruno Alves <alves.bruno945@gmail.com>
2020-06-10 12:58:31 -0300

    Function
    Using a function in Swift

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Functions
+// Function balow not have return and parameter
+func sayHello() {
+    print("Hello")
+}
+
+// Function with parameter
+func sayHello(hello: String) {
+    print(hello)
+}
+
+// Function with return
+func sayHello(hello: String) -> String {
+    return hello
+}
